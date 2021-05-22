312951aee1c0bfe3832e5d91c3b1f20c96d86bf3
Roli <rroycehg@gmail.com>
2021-05-22 16:22:46 -0400

    commit tree 0a4d1627f022c1e2f94c037197a784a4855bbe35

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+//functions to return first two and last two drivers
+function returnFirstTwoDrivers(drivers) {
+    let firstDrivers = drivers.slice(0,2);
+    return firstDrivers;
+}
+
+function returnLastTwoDrivers(drivers) {
+    let lastDrivers = drivers.slice(2,4);
+    return lastDrivers;
+}
+
+//Creates an array to callback the functions that combine both First two and last two drivers
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+//fareMultiplier
+function createFareMultiplier(fareMultiplier) {
+    return function (fareQuintupler) {
+        return fareQuintupler * fareMultiplier;
+    }
+} 
+
+
+//fare double
+const fareDoubler = (fare) => fare*2; 
+
+//fare triple
+const fareTripler = (fare) => fare*3;
+
+
+function selectDifferentDrivers(arrayofDrivers, returnSelectedTwoDrivers){
+    return returnSelectedTwoDrivers(arrayofDrivers);
+}
